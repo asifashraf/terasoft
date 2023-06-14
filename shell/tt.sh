b00c98b946e9501cff31e99de560e7c93abb758c
asif <asif.log@gmail.com>
2023-06-14 02:31:39 +0000

    Added docker commands

--- shell/tt.sh
+++ shell/tt.sh
@@ -4,8 +4,8 @@ action=$1
 
 if [[ -z "$action" || "$action" == "--help" ]]; then
   echo "====== TeraSoft ======"
-  echo "                      cd, push"
-  echo "dk.                   up"
+  echo "                      cd, push, pull, commit"
+  echo "dk.                   up, re, down, in, logs, tail"
 else
  case $action in
   "cd")
@@ -19,9 +19,45 @@ else
       git push
 	;;
 
+  "commit")
+	  source cd-tt.sh
+      git add .
+      git commit -m "$2"
+	;;
+
+  "pull")
+	  source cd-tt.sh
+      git pull
+	;;
+
   "dk.up")
 	  source cd-tt.sh
     docker-compose up -d
+	;;
+
+  "dk.re")
+	  source cd-tt.sh
+    docker-compose restart
+	;;
+
+  "dk.down")
+	  source cd-tt.sh
+    docker-compose down
+	;;
+
+  "dk.in")
+	  source cd-tt.sh
+    docker exec -it ttweb bash
+	;;
+
+  "dk.logs")
+	  source cd-tt.sh
+    docker logs ttweb
+	;;
+
+  "dk.tail")
+	  source cd-tt.sh
+    docker logs -f ttweb
 	;;
 
   *)
